9d663d637797f2504ad97b84cd28e4344f0c1cb2
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-22 15:34:06 +0530

    fmt.scan demo

---
+++ 02-programming-constructs/scan-demo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userName string
+	fmt.Println("Enter your name :")
+	//fmt.Scanln(&userName)
+	fmt.Scanf("%s\n", &userName)
+	//fmt.Println("Hi ", userName, ",Have a nice day")
+	fmt.Printf("Hi %q, Have a nice day!\n", userName)
+
+	var n1, n2 int
+	fmt.Println("Enter 2 numbers")
+	fmt.Scanln(&n1, &n2)
+	//fmt.Scanf("%d %d\n", &n1, &n2)
+	fmt.Println(n1, n2)
+}
