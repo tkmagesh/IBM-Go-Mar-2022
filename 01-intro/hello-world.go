d54eccc28f8f7312f412478e45e6cacad7bd46af
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-22 12:55:14 +0530

    variable declarations

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,16 @@
+/* Package declaration */
+package main
+
+/* import other packages */
+import "fmt"
+
+/* package lever type declarations */
+
+/* package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
---
+++ 01-intro/variables.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+var x = 100
+
+func main() {
+	/*
+		var msg string
+		msg = "Hello World!"
+	*/
+
+	/*
+		var msg string = "Hello World!"
+	*/
+
+	//type inference
+	/*
+		var msg = "Hello World!"
+	*/
+
+	msg := "Hello World!" // := syntax is application only in a function (not at package level)
+	fmt.Println(msg)
+
+	/*
+		var x int
+		var y int
+		var str string
+		var result int
+		x = 100
+		y = 200
+		str = "Result of adding ="
+		result = x + y
+		fmt.Println(x, y, str, result)
+	*/
+
+	/*
+		var x int = 100
+		var y int = 200
+		var str string = "Result of addtion ="
+		var result int = x + y
+		fmt.Println(x, y, str, result)
+	*/
+
+	/* var x, y, result int
+	var str string
+	x = 100
+	y = 200
+	str = "Result of adding ="
+	result = x + y
+	fmt.Println(x, y, str, result) */
+
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		str = "Result of adding ="
+		result = x + y
+		fmt.Println(x, y, str, result)
+	*/
+
+	/*
+		var (
+			x, y   int    = 100, 200
+			str    string = "Result of addition ="
+			result int    = x + y
+		)
+	*/
+
+	/*
+		x, y := 100, 200
+		str := "Result of addition ="
+		result := x + y
+	*/
+
+	x, y, str := 100, 200, "Result of addition ="
+	result := x + y
+
+	fmt.Println(x, y, str, result)
+
+}
