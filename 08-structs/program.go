22ca1adbd7f10bb26ccc61594620553aec63b771
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-24 10:08:56 +0530

    structs intro

---
+++ 08-structs/program.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	Id        int
+	FirstName string
+	LastName  string
+	Salary    float32
+}
+
+func main() {
+	//var emp Employee
+	//var emp Employee = Employee{100, "Magesh", "Kuppan", 10000}
+	//var emp Employee = Employee{Id: 100, FirstName: "Magesh", Salary: 10000}
+	emp := Employee{
+		Id:        100,
+		FirstName: "Magesh",
+		Salary:    10000,
+	}
+	fmt.Printf("%#v\n", emp)
+}
