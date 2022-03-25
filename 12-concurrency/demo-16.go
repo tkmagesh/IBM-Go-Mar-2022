0a32822b0b2056c3e2b3e69328e9567f69e0a832
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 14:19:25 +0530

    closing channels

---
+++ 12-concurrency/demo-16.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//get the generated prime numbers and print them
+
+	ch := genPrimes()
+	for primeNo := range ch {
+		fmt.Println(primeNo)
+	}
+	fmt.Println("Done")
+}
+
+func genPrimes() chan int {
+	count := 30
+	ch := make(chan int)
+	go func() {
+		no := 2
+		for count >= 0 {
+			if isPrime(no) {
+				time.Sleep(300 * time.Millisecond)
+				ch <- no
+				count -= 1
+			}
+			no++
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= no/2; i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
