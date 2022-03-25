014f147bb3ebb02c0bf7301334fdbbadb2dcda6d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 15:33:30 +0530

    assignment-13 solution

--- 12-concurrency/assignment-13.go
+++ 12-concurrency/assignment-13.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	//print the fibonacci series until the user hits ENTER key
+	doneCh := make(chan bool)
+	dataCh := genFibonacci(doneCh)
+	go func() {
+		var input string
+		fmt.Scanln(&input)
+		doneCh <- true
+	}()
+	for no := range dataCh {
+		fmt.Println(no)
+	}
 
-	var input string
-	fmt.Scanln(&input)
 }
 
 /* Keep generating the fibonacci series */
-func genFibonacci() {
-
+func genFibonacci(done chan bool) <-chan int {
+	dataCh := make(chan int)
+	go func() {
+		x, y := 0, 1
+	LOOP:
+		for {
+			select {
+			case <-done:
+				fmt.Println("done")
+				close(dataCh)
+				break LOOP
+			case dataCh <- x:
+				time.Sleep(500 * time.Millisecond)
+				x, y = y, x+y
+			}
+		}
+	}()
+	return dataCh
 }
