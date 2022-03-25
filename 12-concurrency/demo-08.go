30b84dbf99a0ecd89351f30ba2402941b35d7b41
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 11:08:56 +0530

    simulating deadlock

--- 12-concurrency/demo-08.go
+++ 12-concurrency/demo-08.go
@@ -6,14 +6,12 @@ import (
 )
 
 var counter = 0 //race
-var mutex sync.Mutex
 
 func main() {
-
 	wg := &sync.WaitGroup{}
 	fmt.Println("main started")
+	wg.Add(500)
 	for i := 0; i < 100; i++ {
-		wg.Add(1)
 		go fn(wg)
 	}
 	wg.Wait()
@@ -23,9 +21,6 @@ func main() {
 
 func fn(wg *sync.WaitGroup) {
 	defer wg.Done()
-	mutex.Lock()
-	{
-		counter = counter + 1
-	}
-	mutex.Unlock()
+	counter = counter + 1
+	fmt.Println(counter)
 }
