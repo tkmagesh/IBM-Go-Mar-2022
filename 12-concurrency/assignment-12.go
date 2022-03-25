d76f0f6dbf708c99ad0facb648129019a7c41d48
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 12:36:44 +0530

    assignment:12 problem statement

---
+++ 12-concurrency/assignment-12.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//get the generated prime numbers and print them
+}
+
+func genPrimes(count int) {
+	/* keep generating <count> prime numbers from 2 in async mode*/
+}
