0cd09e5cef58ebd16009d25e4feb648635270ee0
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 13:01:45 +0530

    assignment-12 solution

--- 12-concurrency/assignment-12.go
+++ 12-concurrency/assignment-12.go
@@ -1,9 +1,39 @@
 package main
 
+import "fmt"
+
 func main() {
 	//get the generated prime numbers and print them
+	count := 20
+	ch := genPrimes(count)
+	for i := 0; i < count; i++ {
+		fmt.Println(<-ch)
+	}
+	fmt.Println("Done")
 }
 
-func genPrimes(count int) {
-	/* keep generating <count> prime numbers from 2 in async mode*/
+func genPrimes(count int) chan int {
+	ch := make(chan int)
+	go func() {
+		for count >= 0 {
+			no := 2
+			for {
+				if isPrime(no) {
+					ch <- no
+					count -= 1
+				}
+				no++
+			}
+		}
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= no/2; i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
