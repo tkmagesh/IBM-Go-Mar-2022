7ac2e45f261bbaf9d9715241d46e1f2c6d17ed62
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-25 16:09:07 +0530

    buffered channels

--- 12-concurrency/demo-11.go
+++ 12-concurrency/demo-11.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int)
-	go func() {
+	//The following will result in a deadlock
+	/*
+		ch := make(chan int)
 		ch <- 100
-	}()
+		val := <-ch
+		fmt.Println(val) */
+
+	//Solution - 1
+	/*
+		ch := make(chan int)
+		go func(){
+			ch <- 100
+		}()
+		val := <-ch
+		fmt.Println(val)
+	*/
+	//Solution - 2 (using buffered channel)
+	ch := make(chan int, 1)
+	ch <- 100
 	val := <-ch
 	fmt.Println(val)
 }
